84d658e91ab526307510a291fdbe9116d0b6ac65
joeHandsOnRepo <74275293+joeHandsOnRepo@users.noreply.github.com>
2021-09-15 00:14:19 -0500

    Commit Code
    Vm scripts

---
+++ Check-Sql.ps1
@@ -0,0 +1,93 @@
+﻿$Global:Connection = $null
+Function Validate-ODBCDriver($Server, $DB)
+{
+    if([Bool](Get-OdbcDsn -Name "$DB" -DsnType "System" -Platform "64-bit" -EA Ignore))
+    {
+        Write-Verbose -Message "ValidateODBCDriver: Driver Found & Healthy" -Verbose
+        Return $true
+    }
+    else
+    {
+        Write-Warning -Message "ValidateODBCDriver: Driver Not Found" -Verbose
+        Add-OdbcDsn -Name "$DB" -DriverName "SQL Server" -DsnType "System" -Platform '64-bit' `
+        -SetPropertyValue @("Server=$Server","Trusted_Connection=Yes","Database=$DB") -PassThru -ErrorAction Stop
+        Write-Verbose -Message "ValidateODBCDriver: Driver Created Successfully" -Verbose
+        Return $true
+    }
+}
+
+Function SQLServerConnection($Server, $DB)
+{
+    if ($Global:Connection -eq $null)
+    {
+        Write-Warning -Message "SQLServerConnection: Connection Lost" -Verbose
+        Write-Verbose -Message "SQLServerConnection: Establishing Conection..." -Verbose
+        $Global:Connection = New-Object System.Data.Odbc.OdbcConnection
+        $Global:Connection.ConnectionString = "Driver={SQL Server};Server=$Server;Database=$DB"
+        $Global:Connection.Open()
+        Write-Host "Connection Eastablished Successfully" -ForegroundColor Green
+    }
+    else
+    {
+        Write-Verbose -Message "SQLServerConnection: Connection Is Alive" -Verbose
+        Return $Global:Connection
+    }
+}
+
+Function SQLSelection($Query)
+{
+    $CMD = New-Object System.Data.Odbc.OdbcCommand($Query,$Global:Connection)
+    $DS = New-Object system.Data.DataSet
+    (New-Object system.Data.odbc.odbcDataAdapter($CMD)).Fill($DS) | Out-Null
+    $DS.Tables[0]
+}
+
+Try
+{
+    # Give The Server Name
+    $Server = "AZ1WKSTN001";
+    # Don't Change The DB Name
+    $DB = "master";
+
+    $Check = Validate-ODBCDriver -Server $Server -DB $DB
+    if($Check)
+    {
+        SQLServerConnection -Server $Server -DB $DB
+        Write-Host "OSVersion"
+        [System.Environment]::OSVersion | FT
+        Write-Host "VM Name"
+        HostName | FT
+        $Query = "Select @@version As SqlVersion"
+        $Result = SQLSelection -Query $Query
+        $Result | FT -Wrap
+
+        $Query = "Select @@servicename As InstanceName"
+        $Result = SQLSelection -Query $Query
+        $Result | FT -AutoSize
+
+        $Query = "Select COUNT(*) DatabasesCount FROM sys.databases"
+        $Result = SQLSelection -Query $Query
+        $Result | FT -AutoSize
+
+        $Query = "Select serverproperty('Edition') As SkuName"
+        $Result = SQLSelection -Query $Query
+        $Result | FT -AutoSize
+
+        $Query = "declare @rc int, @dir nvarchar(4000) exec @rc = master.dbo.xp_instance_regread 
+                 N'HKEY_LOCAL_MACHINE', 
+                 N'Software\Microsoft\MSSQLServer\Setup', 
+                 N'SQLPath', @dir output, 'no_output' select @dir AS InstallationDirectory"
+        $Result = SQLSelection -Query $Query
+        $Result | FT -AutoSize
+    }
+}
+Catch
+{
+    $Message = $_.Exception.Message
+    Write-Warning -Message $Message -Verbose
+}
+Finally
+{
+    $Global:Connection.Close()
+    Write-Host "Connection Closed Successfully" -ForegroundColor Green
+}
---
+++ ListVMsInformationsValidated.ps1
@@ -0,0 +1,117 @@
+# First you need to install module ImportExcel
+# Install-Module ImportExcel -Scope CurrentUser
+
+$ErrorActionPreference = "stop"
+Write-Host "Getting All Virtual Machines..."
+$WindowsRunningVMs = get-azvm -status 
+
+$CommonVmsInfo = @()
+$DataDisksInfo = @()
+$NetworkInterfaceInfo = @()
+foreach ($vm in $WindowsRunningVMs) {
+    $currentVM = get-azvm -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName
+    #------------------- Getting VMs Information Details -------------------
+    $VMName = $currentVM.Name
+    $VMResourceGroupName = $currentVM.ResourceGroupName
+    $VMStatus = $vm.PowerState
+    $VMSize =  $currentVM.HardwareProfile.VmSize
+
+    $VMComputerName = $currentVM.OSProfile.ComputerName
+    $VMAdmineUserName = $currentVM.OSProfile.AdminUsername
+   
+    $VMOsType = $currentVM.StorageProfile.OsDisk.OsType
+    $VMPublisher = $currentVM.StorageProfile.ImageReference.Publisher
+    $VMRelease = $currentVM.StorageProfile.ImageReference.sku
+    $VMReleaseVersion = $currentVM.StorageProfile.ImageReference.version
+
+    $OsDiskName = $currentVM.StorageProfile.OsDisk.Name
+    $OsDiskSizeGB = $currentVM.StorageProfile.OsDisk.DiskSizeGB
+
+    Write-Host "VM Name: $($currentVM.Name)`tStatus: $($vm.PowerState)"
+    $CommonVmsInfo_object = New-Object PSObject
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMName" -Value $VMName
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMResourceGroupName" -Value $VMResourceGroupName
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMStatus" -Value $VMStatus
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMSize" -Value $VMSize
+
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMComputerName" -Value $VMComputerName
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMAdmineUserName" -Value $VMAdmineUserName
+
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMOsType" -Value $VMOsType
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMPublisher" -Value $VMPublisher
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMRelease" -Value $VMRelease
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "VMReleaseVersion" -Value $VMReleaseVersion
+
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "OsDiskName" -Value $OsDiskName
+    $CommonVmsInfo_object | Add-Member -MemberType NoteProperty -Name "OsDiskSizeGB" -Value $OsDiskSizeGB
+
+    $CommonVmsInfo += $CommonVmsInfo_object
+
+    #------------------- Get all data disks -------------------
+    foreach ($dataDisk in $currentVM.StorageProfile.DataDisks) {
+        $dataDiskName = $dataDisk.Name
+        $CurrentDataDisk = Get-AzDisk -ResourceGroupName $currentVM.ResourceGroupName -DiskName $dataDisk.Name
+        $dataDisksizeGB = $currentDataDisk.DiskSizeGB
+        $dataDiskSku = $currentDataDisk.sku.Name
+
+        Write-Host "VM Name: $($vm.Name)`tDisk Name: $dataDiskName`tSKU: $dataDiskSku" -ForegroundColor Yellow
+        $DataDisksInfo_object = New-Object PSObject
+        $DataDisksInfo_object | Add-Member -MemberType NoteProperty -Name "Disk Name" -Value $dataDiskName
+        $DataDisksInfo_object | Add-Member -MemberType NoteProperty -Name "VM Name" -Value $currentVM.Name
+        $DataDisksInfo_object | Add-Member -MemberType NoteProperty -Name "size GB" -Value $dataDisksizeGB
+        $DataDisksInfo_object | Add-Member -MemberType NoteProperty -Name "SKU" -Value $dataDiskSku
+
+        $DataDisksInfo += $DataDisksInfo_object
+    }
+
+
+    #------------------- Get all IP Configurations / NIC -------------------
+    $VMNetworkInterfaces = $currentVM.NetworkProfile.NetworkInterfaces
+    # Check first if there a nic attached to the vm
+    if ($null -ne $VMNetworkInterfaces) {
+        foreach($nic in $VMNetworkInterfaces) {
+
+            $NicId = $nic.id
+            $NicName = $NicId.split('/')[-1]
+            $CurrentNic = Get-AzNetworkInterface -Name $NicName -ResourceGroupName $currentVM.ResourceGroupName
+            #Getting Public and Private IP Addresses:
+            $PrivateIPList = ""
+            $PublicIPList = ""
+            foreach ($IPConfig in $CurrentNic.IpConfigurations) {
+                $PrivateIPList = $PrivateIPList + " " + $IPConfig.PrivateIpAddress
+                if ($null -ne $PubIPConfig.PublicIpAddress) {
+                    $CurrentPublicIPName = ($IPConfig.PublicIpAddress.Id).split('/')[-1]
+                    $CurrentPublicIPRG = ($IPConfig.PublicIpAddress.Id).split('/')[4]
+                    $CurrentPublicIP = (Get-AzPublicIpAddress -Name $CurrentPublicIPName -ResourceGroupName $CurrentPublicIPRG).IpAddress
+                    $PublicIPList = $PublicIPList + " " + $CurrentPublicIP
+                }
+                else {
+                    $PublicIPList = $PublicIPList + " " + "None"
+                }
+            }
+            if ($true -eq $CurrentNic.EnableAcceleratedNetworking) {
+                $AcceleratedNetworking = "Enabled"
+            }
+            else {
+                $AcceleratedNetworking = "Disabled"
+            }
+
+            Write-Host "VM Name: $($vm.Name)`tNIC Name: $NicName" -ForegroundColor Yellow
+            $NIC_object = New-Object PSObject
+            $NIC_object | Add-Member -MemberType NoteProperty -Name "NIC Name" -Value $NicName
+            $NIC_object | Add-Member -MemberType NoteProperty -Name "VM Name" -Value $currentVM.Name
+            $NIC_object | Add-Member -MemberType NoteProperty -Name "Accelerated Networking" -Value $AcceleratedNetworking
+            $NIC_object | Add-Member -MemberType NoteProperty -Name "Private IPs" -Value $PrivateIPList
+            $NIC_object | Add-Member -MemberType NoteProperty -Name "Public IPs" -Value $PublicIPList
+
+            $NetworkInterfaceInfo += $NIC_object
+        }
+    }
+    else {
+            write-Host "The is no Network Interface Card attached to the vm '$($currentVM.Name)'"
+    }
+}
+
+$CommonVmsInfo | Export-Excel "VmsInformation.xlsx" -WorksheetName "VMs Common Information"
+$DataDisksInfo | Export-Excel "VmsInformation.xlsx" -WorksheetName "Data Disks Information"
+$NetworkInterfaceInfo | Export-Excel "VmsInformation.xlsx" -WorksheetName "VMs Network Information"
